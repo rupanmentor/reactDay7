e017c80f156add800bc7821b3fa0565b99aa9796
rupanmentor <rupanguvimentor@gmail.com>
2025-10-30 19:40:09 +0530

    router half completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Products from "./Pages/Products";
+import PageNotFound from "./Pages/PageNotFound";
 
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <div>
+          <Navbar />
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+        <div>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </>
   );
 };
 
